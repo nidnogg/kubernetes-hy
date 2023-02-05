f1dddf4e370c2eda14005e81ecb19d7edc93f49b
nidnogg <henriquevt98@gmail.com>
2023-02-05 00:53:43 -0300

    Added exercise 1.01

---
+++ part1/log-output/src/main.rs
@@ -0,0 +1,32 @@
+// use axum_server::Handle;
+use std::time::{Duration};
+use time;
+use std::thread::sleep;
+
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+/// This app prints out a randomly generated
+/// `String` accompanied by a timestamp every 5 seconds
+fn main() {
+    main_cycle()
+}
+
+fn main_cycle() {
+    let random_string = random_string(10);
+    loop {
+        let timestamp = time::OffsetDateTime::now_utc();
+        // Sleep every 5 seconds
+        sleep(Duration::new(5, 0));
+        println!("{}: {}", timestamp, random_string);
+    }
+}
+
+fn random_string(n: usize) -> String {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(n)
+        .map(char::from)
+        .collect();
+    rand_string
+}
